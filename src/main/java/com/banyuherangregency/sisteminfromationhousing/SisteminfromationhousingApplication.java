75a7b813a459b7bf6750fb6d0eee5c9b879c8b08
Zaki-Fadlan <zakifadlan3300@gmail.com>
2023-07-11 01:56:06 +0700

    first commit

---
+++ src/main/java/com/banyuherangregency/sisteminfromationhousing/SisteminfromationhousingApplication.java
@@ -0,0 +1,13 @@
+package com.banyuherangregency.sisteminfromationhousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisteminfromationhousingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisteminfromationhousingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banyuherangregency/sisteminfromationhousing/SisteminfromationhousingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banyuherangregency.sisteminfromationhousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisteminfromationhousingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
